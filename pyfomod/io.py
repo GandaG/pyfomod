5a0700e0b57fb97915a6045c150a57be799fb1dc
Daniel Nunes <gandaganza@gmail.com>
2017-01-28 18:57:15 +0000

    Added io module. Added get_installer_files function and tests.

---
+++ pyfomod/io.py
@@ -0,0 +1,65 @@
+# Copyright 2016 Daniel Nunes
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+
+def get_installer_files(search_path):
+    """
+    Returns a tuple of (info.xml, ModuleConfig.xml) as paths.
+    `search_path` is the path to search for these files at.
+    `search_path` must point to a folder.
+
+    Both a subfolder named *fomod* in the `search_path` and
+    `search_path` itself being the *fomod* folder are supported.
+    Priority is given to *fomod* subfolder.
+    `search_path` examples:
+
+    * `folder/somefolder/` - *somefolder* contains a *fomod* subfolder;
+    * `folder/somefolder/fomod`.
+
+    Raises IOError if there are any issues with finding files or folder.
+    """
+
+    # normalize path to get rid of trailing stuff and properly set slashes
+    os.path.normpath(search_path)
+
+    # if the path doesn't exist just raise IOError (py2 compat)
+    if not os.path.isdir(search_path):
+        raise IOError("search_path provided does not exist or is not a folder")
+
+    # look for a fomod subfolder
+    fomod_path = os.path.join(search_path, 'fomod')
+    if not os.path.isdir(fomod_path):
+        fomod_path = ''  # not found, reset and continue
+
+    # check if the search_path is a fomod folder
+    if os.path.basename(search_path) == 'fomod' and not fomod_path:
+        fomod_path = search_path
+
+    # if fomod_path hasn't been found, error out
+    if not fomod_path:
+        raise IOError("fomod folder not found")
+
+    # grab the files and check if they exist
+    info_path = os.path.join(fomod_path, 'info.xml')
+    if not os.path.isfile(info_path):
+        raise IOError("info.xml does not exist in the fomod folder")
+
+    config_path = os.path.join(fomod_path, 'ModuleConfig.xml')
+    if not os.path.isfile(config_path):
+        raise IOError("ModuleConfig.xml does not exist in the fomod folder")
+
+    # both files exist, life is good
+    return info_path, config_path
---
+++ tests/test_io.py
@@ -0,0 +1,80 @@
+import os
+
+import pytest
+import requests
+from pyfomod import io
+
+
+@pytest.fixture
+def valid_fomod(tmpdir):
+    """
+    Very simply, return the path to the root of a valid
+    fomod-containing package.
+    """
+    fomod_path = os.path.join(str(tmpdir), 'fomod')
+    os.mkdir(fomod_path)
+
+    info_path = os.path.join(fomod_path, 'info.xml')
+    info_url = ("https://raw.githubusercontent.com/fomod-lang/fomod/"
+                "master/examples/minimal/fomod/info.xml")
+    with open(info_path, 'wb') as info_file:
+        file_content = requests.get(info_url).content
+        info_file.write(file_content)
+
+    config_path = os.path.join(fomod_path, 'ModuleConfig.xml')
+    config_url = ("https://raw.githubusercontent.com/fomod-lang/fomod/"
+                  "master/examples/minimal/fomod/ModuleConfig.xml")
+    with open(config_path, 'wb') as config_file:
+        file_content = requests.get(config_url).content
+        config_file.write(file_content)
+
+    return str(tmpdir)
+
+
+@pytest.fixture
+def missing_info_fomod(valid_fomod):
+    """
+    Returns a fomod-containing package with a missing
+    info.xml file.
+    """
+    info_path = os.path.join(valid_fomod, 'fomod', 'info.xml')
+    os.remove(info_path)
+    return valid_fomod
+
+
+@pytest.fixture
+def missing_config_fomod(valid_fomod):
+    """
+    Returns a fomod-containing package with a missing
+    ModuleConfig.xml file.
+    """
+    config_path = os.path.join(valid_fomod, 'fomod', 'ModuleConfig.xml')
+    os.remove(config_path)
+    return valid_fomod
+
+
+class Test_get_installer_files:
+    def test_empty(self, tmpdir):
+        with pytest.raises(IOError):
+            # mimic a package that does not contain a fomod folder
+            io.get_installer_files(str(tmpdir))
+
+    def test_no_fomod_folder(self, tmpdir):
+        with pytest.raises(IOError):
+            # mimic a fake path
+            fake_path = os.path.join(str(tmpdir), 'fomod')
+            io.get_installer_files(fake_path)
+
+    def test_missing_info(self, missing_info_fomod):
+        with pytest.raises(IOError):
+            io.get_installer_files(missing_info_fomod)
+
+    def test_missing_config(self, missing_config_fomod):
+        with pytest.raises(IOError):
+            io.get_installer_files(missing_config_fomod)
+
+    def test_valid(self, valid_fomod):
+        fomod_path = os.path.join(valid_fomod, 'fomod')
+        base_files = io.get_installer_files(valid_fomod)
+        fomod_files = io.get_installer_files(fomod_path)
+        assert base_files == fomod_files
