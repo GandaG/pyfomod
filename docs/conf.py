79d598a4893dd123c100a6a3a37cd734011d1386
Daniel Nunes <daniel.henri.nunes@gmail.com>
2018-12-27 16:08:45 +0000

    Bump version: 0.4.0 → 0.5.0

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ author = 'Daniel Nunes'
 # built documents.
 #
 # The short X.Y version.
-version = '0.4.0'
+version = '0.5.0'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- pyfomod/__init__.py
+++ pyfomod/__init__.py
@@ -14,7 +14,7 @@
 
 """A high-level fomod library written in Python."""
 
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 __all__ = [
     "Conditions",
