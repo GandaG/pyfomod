d86f84fe9a542a4301baea9c9702e8d45b102a68
Daniel Nunes <daniel.henri.nunes@gmail.com>
2019-03-07 16:17:40 +0000

    Bump version: 1.0.0 → 1.0.1

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ author = 'Daniel Nunes'
 # built documents.
 #
 # The short X.Y version.
-version = '1.0.0'
+version = '1.0.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
