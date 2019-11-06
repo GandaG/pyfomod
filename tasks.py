9c27b5c3a5a661233d99c6caa4110ac74d9cd72c
Daniel Nunes <daniel.henri.nunes@gmail.com>
2019-11-06 20:10:44 +0000

    Improve task running
    Removed the pytest-sugar that was causing problems with windows
    terminals.
    
    The changes to tasks.py should fix the issues with not all tasks running
    with 'check'.

--- tasks.py
+++ tasks.py
@@ -8,14 +8,26 @@ ROOT_FOLDER = Path(__file__).resolve().parent
 
 
 @task
-def check(c):
+def isort(c):
     c.run("isort -m 3 -tc -fgw 0 -up -w 88 -rc src/pyfomod tests tasks.py")
-    c.run("isort -m 3 -tc -fgw 0 -up -w 88 -rc -ns __init__.py src/pyfomod/__init__.py")
-    c.run("black -q -t py36 src/pyfomod tests tasks.py")
+
+
+@task
+def black(c):
+    c.run("black src/pyfomod tests tasks.py")
+
+
+@task
+def flake(c):
     c.run(
         "flake8 --max-line-length=80 --select=C,E,F,W,B,B950 "
-        "--ignore=B305,E203,E501,W503 src/pyfomod tests tasks.py"
+        "--ignore=B305,E203,E501,E731,W503 src/pyfomod tests tasks.py"
     )
+
+
+@task(pre=[isort, black, flake])
+def check(c):
+    pass
 
 
 @task
